06e30b74045c2142313b92ce365e8e1c6e81b17e
34j <34j.github@proton.me>
2025-09-22 17:45:14 +0900

    docs: wip

--- tests/test_dunder_main.py
+++ tests/test_dunder_main.py
@@ -5,7 +5,7 @@ import sys
 def test_can_run_as_python_module():
     """Run the CLI as a Python module."""
     result = subprocess.run(  # noqa: S603
-        [sys.executable, "-m", "sph_harm", "--help"],
+        [sys.executable, "-m", "ultrasphere_harmonics", "--help"],
         check=True,
         capture_output=True,
     )
